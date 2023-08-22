8038abef6ab9af988b742dcb669bbcfab58db079
Shivani Pal <shiv040998@gmail.com>
2023-08-22 15:07:45 -0400

    rename

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          WorldVibe
+          HashTag
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
